17a78d8035103d5a638d68469e55f676bedb5268
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-02-08 22:33:35 +0700

    first commit

---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,30 @@
+import React, {useEffect} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {Logo} from '../../assets';
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+    }, 2000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <View style={{height: 30}}/>
+      <Text style={styles.text}>ShopAroundMe</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2D929A',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {fontSize: 32, color: '#ffffff'},
+});
